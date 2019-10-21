add82d0e8bc771b34fc7e8f036b68d845608ce09
khawarzia <khawarzia34@gmail.com>
2019-10-22 01:16:29 +0500

    third

--- profileinfo/admin.py
+++ profileinfo/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import follow,post,message,comment,comment_child
+from .models import follow,post,message,comment,comment_child,notifications
 
 admin.site.register(follow)
 admin.site.register(post)
 admin.site.register(message)
 admin.site.register(comment)
 admin.site.register(comment_child)
+admin.site.register(notifications)
