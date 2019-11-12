ca4a02c439552caeccef7427a6ce46c0ad574a2f
khawarzia <khawarzia34@gmail.com>
2019-11-12 19:58:00 +0500

    first

--- profileinfo/signals.py
+++ profileinfo/signals.py
@@ -8,13 +8,24 @@ from django.core.mail import EmailMessage
 @receiver(post_save,sender=post)
 def email_send(sender,instance,**kwargs):
     if instance.approved_by_admin:
-        email_add = instance.user.email
-        a = EmailMessage(
-            subject='LibriCK Approved by Admin',
-            body='Your LibriCK with title : '+str(instance.titledisplay)+' has been approved by the admin.\nView your post now : https://127.0.0.1:8000/post/'+str(instance.titledisplay),
-            to = [email_add]
-        )
-        a.send()
+        #email_add = instance.user.email
+        #a = EmailMessage(
+        #    subject='LibriCK Approved by Admin',
+        #    body='Your LibriCK with title : '+str(instance.titledisplay)+' has been approved by the admin.\nView your post now : https://127.0.0.1:8000/post/'+str(instance.titledisplay),
+        #    to = [email_add]
+        #)
+        #a.send()
+        a = 'Your LibriCK with title : '+str(instance.title)+' has been approved by the admin.\nView your post now : https://67.207.92.234:8000/post/'+str(instance.permalink)
+        objs = notifications.objects.all()
+        for i in objs:
+            if i.notification == a:
+                return
+        obj = notifications()
+        obj.user = instance.user
+        obj.notification = a
+        obj.save()
+        obj.relpost.add(instance)
+        obj.save()
     else:
         pass
     return
@@ -27,6 +38,10 @@ def amazon_code(sender, instance, **kwargs):
             if i.user == instance.user:
                 linked = i
                 break
+        objs = notifications.objects.all()
+        for i in objs:
+            if i.user == instance.user and a.notification == 'Congratulazioni, la copertina del tuo Libro è stata aggiunta ai tuo LibriCK!':
+                return
         a = notifications()
         a.user = instance.user
         a.notification = 'Congratulazioni, la copertina del tuo Libro è stata aggiunta ai tuo LibriCK!'
