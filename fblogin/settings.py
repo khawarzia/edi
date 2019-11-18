847f5aa88d32efac0654251d8bebf10b61e22f0e
Khawar Zia <53947398+khawarzia@users.noreply.github.com>
2019-11-18 23:44:03 +0500

    Update settings.py

--- fblogin/settings.py
+++ fblogin/settings.py
@@ -151,5 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'testingdjango4@gmail.com'
-EMAIL_HOST_PASSWORD = 'djangotest111'
+EMAIL_HOST_USER = 'futuresoftcode@gmail.com'
+EMAIL_HOST_PASSWORD = 'Sul03314307703'
