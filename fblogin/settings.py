93df7bb34d5958c3a8693e30d4eb64881600526c
Khawar Zia <53947398+khawarzia@users.noreply.github.com>
2019-11-28 01:02:26 +0500

    Update settings.py

--- fblogin/settings.py
+++ fblogin/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'gw-j72sl#jpt80)r3a9*ely$i8xsax_4k!b@ue1@i@5cvq1(+3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['67.207.92.234','127.0.0.1']
+ALLOWED_HOSTS = ['165.227.48.28','127.0.0.1']
 
 
 # Application definition
